9de618c45de78a65407d7de9fe1787ff4fdba3d4
Nathan-Vinh <90573409+Nathan-Vinh@users.noreply.github.com>
2023-04-28 08:55:57 +0200

    Update dash_callback.py

--- dash_callback.py
+++ dash_callback.py
@@ -5,9 +5,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import Perceptron
 import pandas as pd
-#import tensorflow as tf
-#from tensorflow.keras.preprocessing.text import Tokenizer
-#from tensorflow.keras.preprocessing.sequence import pad_sequences
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 @callback(
     [
@@ -62,24 +62,30 @@ def sklearnmodel(tfb, skb, text):
 
 
     elif 'tf-button' == ctx.triggered_id:
-        # model = tf.keras.Sequential([
-        #     tf.keras.layers.Embedding(input_dim=1000, output_dim=64, input_length=100),
-        #     tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'),
-        #     tf.keras.layers.MaxPooling1D(pool_size=2),
-        #     tf.keras.layers.Flatten(),
-        #     tf.keras.layers.Dense(units=10, activation='relu'),
-        #     tf.keras.layers.Dense(units=1, activation='relu')
-        # ])
+        model = tf.keras.Sequential([
+             tf.keras.layers.Embedding(input_dim=1000, output_dim=64, input_length=100),
+             tf.keras.layers.Conv1D(filters=64, kernel_size=3, activation='relu'),
+             tf.keras.layers.MaxPooling1D(pool_size=2),
+             tf.keras.layers.Flatten(),
+             tf.keras.layers.Dense(units=10, activation='relu'),
+             tf.keras.layers.Dense(units=1, activation='relu')
+        ])
 
-        # model.compile(loss='binary_crossentropy',
-        #               optimizer='adam', metrics=['accuracy'])
-        # callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
-        # history = model.fit(tfidf_train, y_train, epochs=10,
-        #                     validation_data=(tfidf_test, y_test), callback=[callback])
+        model.compile(loss='binary_crossentropy',
+                       optimizer='adam', metrics=['accuracy'])
+        callback = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=3)
+        history = model.fit(tfidf_train, y_train, epochs=10,
+                             validation_data=(tfidf_test, y_test), callbacks=[callback])
+        
+        input_no_punct = remove_punct(text)
+        input_tokenized = toknize(input_no_punct)
+        input_no_sw = remove_stopwords(input_tokenized)
+        input_lem = lemmatizing(input_no_sw)
 
-        # result = model.predict()
+        input_vec = tfidf_vect_fit.transform([input_lem])
 
-        result= "model not loaded"
+        result = model.predict(input_vec)
+
 
         if result == 'ham':
             return [html.H4(f"With TensorFlow model, your message is a {result}", style={'color': 'green'})]
